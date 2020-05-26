760fd3b58626136e9a82598f8abc7aac2074b78d
Akmal <akmalrusli363@outlook.com>
2020-05-26 22:50:28 +0700

    Optimized css nav alignment and footer positioning

--- assets/script/navigation.js
+++ assets/script/navigation.js
@@ -1,9 +1,27 @@
 var navMenu = document.getElementById('navMenu');
+var nav = document.getElementById('navigationBar');
+
+navMenu.style.right = (navMenu.clientWidth - navMenu.offsetWidth) + "px";
+
+if (window.pageYOffset == 0 && nav.classList.contains("blend")) {
+  nav.classList.add("transparent");
+}
 
 function peekDropdown() {
   if (navMenu.className === "menu") {
-    navMenu.className += " responsive";
+    navMenu.classList.add("responsive");
+    nav.classList.remove("transparent");
   } else {
-    navMenu.className = "menu";
+    navMenu.classList.remove("responsive");
+    if (window.pageYOffset == 0 && nav.classList.contains("blend"))
+      nav.classList.add("transparent");
   }
 }
+
+window.onscroll = function () {
+  if (window.pageYOffset == 0 && nav.classList.contains("blend")) {
+    nav.classList.add("transparent");
+  } else {
+    nav.classList.remove("transparent");
+  }
+};
