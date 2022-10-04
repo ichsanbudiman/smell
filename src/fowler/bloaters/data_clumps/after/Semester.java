d41c447f0ab13118c7e1931cef90ba54b7765ea3
ichsanbudiman <budimanichsan@gmail.com>
2022-10-05 03:59:14 +0700

    Update Semester.java

--- src/fowler/bloaters/data_clumps/after/Semester.java
+++ src/fowler/bloaters/data_clumps/after/Semester.java
@@ -4,25 +4,19 @@ import java.util.Date;
 
 public class Semester {
 	private String label;
-	private Date start;
-	private Date end;
+	private DateRange dateRange;
 	
 	public Semester(String label, Date start, Date end) {
 		super();
 		this.label = label;
-		this.start = start;
-		this.end = end;
+		this.dateRange = new DateRange(start,end);
 	}
 	
 	public String getLabel() {
 		return label;
 	}
 	
-	public Date getStart() {
-		return start;
-	}
-	
-	public Date getEnd() {
-		return end;
+	public Date getDateRange() {
+		return dateRange;
 	}
 }
